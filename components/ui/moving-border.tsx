7f33480232ca86ddb35929bced24507ca8c45e71
Emmanuel Okorie <emmanuel.okorie@tm30.net>
2025-01-22 13:39:23 +0100

    fixes

--- components/ui/moving-border.tsx
+++ components/ui/moving-border.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { RefObject } from "react";
+import React from "react";
 import {
   motion,
   useAnimationFrame,
@@ -22,7 +22,7 @@ export function Button({
 }: {
   borderRadius?: string;
   children: React.ReactNode;
-  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>;
+  as?: any;
   containerClassName?: string;
   borderClassName?: string;
   duration?: number;
@@ -82,7 +82,7 @@ export const MovingBorder = ({
   ry?: string;
   [key: string]: any;
 }) => {
-  const pathRef = useRef<SVGRectElement | null>(null);
+  const pathRef = useRef<any>();
   const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
@@ -95,11 +95,11 @@ export const MovingBorder = ({
 
   const x = useTransform(
     progress,
-    (val) => pathRef.current?.getPointAtLength(val).x || 0
+    (val) => pathRef.current?.getPointAtLength(val).x
   );
   const y = useTransform(
     progress,
-    (val) => pathRef.current?.getPointAtLength(val).y || 0
+    (val) => pathRef.current?.getPointAtLength(val).y
   );
 
   const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
